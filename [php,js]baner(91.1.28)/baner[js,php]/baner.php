50de6455d27489f390eeba73e026374c86dd2742
fateme-norani <nfnf60@gmail.com>
2012-04-16 15:10:16 +0430

    baner php va js

---
+++ [php,js]baner(91.1.28)/baner[js,php]/baner.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>my banner</title>
+<link type="text/css" rel="stylesheet" href="baner.css" />
+<script type="text/javascript" src="baner.js" ></script>
+</head>
+<body>
+<div class="contaner">
+	<div class="baner">
+    	<div class="scrol">
+        	<div class="dv1"></div>
+            <div class="dv2"></div>
+            <div class="dv3"></div>
+            <div class="dv4"></div>
+            <div class="dv5"></div>
+		</div>
+        <ul class="btn">
+        	<li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul> 
+    </div>
+    <div class="dokme">
+		<input type="button"  id="back"  class="bak" onclick="backslid()"/>
+        <input type="button"  id="next"  class="nxt" onclick="nextslid()"/>
+    </div>   
+</div> 
+</body>           
+</html>
+<?php 
+				$d=dir('./images');
+				while($f=$d->read()){
+					if(strtolower(substr($f,-4))!='.jpg') continue;
+					echo "<img src='images/$f' />";	
+				}
+			?>
